e41ccc3d4a0b6e9138f454800f4cd7aa67260481
Stephen Cross <stephen.cross@bristol.ac.uk>
2017-03-17 17:09:40 +0000

    Added fluorescence channel tracking
    Uses analyse particles on a thresholded image.  
    
    Cells are stored as Rois (class extended from).

--- src/main/java/wolfson/RelateCells/Main.groovy
+++ src/main/java/wolfson/RelateCells/Main.groovy
@@ -1,22 +1,40 @@
 package wolfson.RelateCells
 
-import fiji.IJ1LogService
 import fiji.plugin.trackmate.Logger
 import fiji.plugin.trackmate.Model
 import fiji.plugin.trackmate.Settings
+import fiji.plugin.trackmate.Spot
+import fiji.plugin.trackmate.SpotCollection
 import fiji.plugin.trackmate.TrackMate
 import fiji.plugin.trackmate.detection.DetectorKeys
 import fiji.plugin.trackmate.detection.LogDetectorFactory
+import fiji.plugin.trackmate.tracking.LAPUtils
 import fiji.plugin.trackmate.tracking.TrackerKeys
-import fiji.plugin.trackmate.tracking.sparselap.SparseLAPTrackerFactory
+import fiji.plugin.trackmate.tracking.oldlap.SimpleLAPTrackerFactory
 import ij.IJ
 import ij.ImageJ
 import ij.ImagePlus
+import ij.gui.OvalRoi
+import ij.gui.PolygonRoi
+import ij.gui.Roi
+import ij.measure.Measurements
+import ij.measure.ResultsTable
 import ij.plugin.ChannelSplitter
 import ij.plugin.Duplicator
+import ij.plugin.HyperStackConverter
 import ij.plugin.ImageCalculator
+import ij.plugin.filter.BackgroundSubtracter
 import ij.plugin.filter.GaussianBlur
+import ij.plugin.filter.ParticleAnalyzer
+import ij.plugin.frame.RoiManager
+import ij.process.AutoThresholder
+import ij.process.FloatPolygon
 import ij.process.ImageConverter
+import ij.process.StackStatistics
+
+import java.awt.Polygon
+import java.lang.reflect.Array
+
 
 /**
  * Created by sc13967 on 14/03/2017.
@@ -24,6 +42,11 @@ import ij.process.ImageConverter
 class Main {
     // Getting open image
     static void main(String[] args) {
+        new Main().run()
+
+    }
+
+    void run(){
         // Parameters
         def dogR = 4
 
@@ -40,13 +63,15 @@ class Main {
         // Calculating the difference of Gaussian for the phase-contrast channel
         def phaseSub = runDoG(ipls[1],dogR)
 
-        phaseSub.show()
+        // Detecting cells in the phase-contrast channel
+//         ArrayList<Cell> phaseCells = detectPhaseContrastCells(phaseSub)
 
-        runTrackMate(phaseSub)
+        // Detecting cells in the fluorescence channel
+        ArrayList<Cell> fluoCells = detectFluorescentCells(ipls[0])
 
     }
 
-    static ImagePlus runDoG(ImagePlus ipl, double sigma) {
+    ImagePlus runDoG(ImagePlus ipl, double sigma) {
         // Duplicating the input ImagePlus
         def ipl1 = ipl
         def ipl2 = new Duplicator().run(ipl1)
@@ -68,38 +93,135 @@ class Main {
         // Subtracting one from the other to give the final DoG result
         def iplOut = new ImageCalculator().run("Subtract create 32-bit",ipl2,ipl1)
 
+        new ImageConverter(iplOut).convertToGray8()
+
         return iplOut
 
     }
 
-    static void runTrackMate(ImagePlus ipl) {
+    ArrayList<Cell> detectPhaseContrastCells(ImagePlus ipl) {
+        def model = new Model()
+        model.setLogger(Logger.IJ_LOGGER)
+
         def settings = new Settings()
         settings.setFrom(ipl)
 
         settings.detectorFactory = new LogDetectorFactory()
         settings.detectorSettings.put(DetectorKeys.KEY_DO_SUBPIXEL_LOCALIZATION,true)
-        settings.detectorSettings.put(DetectorKeys.KEY_RADIUS,2.5)
-        settings.detectorSettings.put(DetectorKeys.KEY_TARGET_CHANNEL,0)
-        settings.detectorSettings.put(DetectorKeys.KEY_THRESHOLD,0.001)
+        settings.detectorSettings.put(DetectorKeys.KEY_RADIUS,(Double) 7.5)
+        settings.detectorSettings.put(DetectorKeys.KEY_TARGET_CHANNEL,(Integer) 1)
+        settings.detectorSettings.put(DetectorKeys.KEY_THRESHOLD,(Double) 2)
         settings.detectorSettings.put(DetectorKeys.KEY_DO_MEDIAN_FILTERING,false)
 
-        settings.trackerFactory = new SparseLAPTrackerFactory()
-        settings.trackerSettings.put(TrackerKeys.KEY_GAP_CLOSING_MAX_DISTANCE,1d)
-        settings.trackerSettings.put(TrackerKeys.KEY_LINKING_MAX_DISTANCE,1d)
-        settings.trackerSettings.put(TrackerKeys.KEY_GAP_CLOSING_MAX_FRAME_GAP,1d)
+        settings.trackerFactory = new SimpleLAPTrackerFactory()
+        settings.trackerSettings = LAPUtils.getDefaultLAPSettingsMap()
+        settings.trackerSettings.put(TrackerKeys.KEY_GAP_CLOSING_MAX_DISTANCE,(Double) 1)
+        settings.trackerSettings.put(TrackerKeys.KEY_LINKING_MAX_DISTANCE,(Double) 1)
+        settings.trackerSettings.put(TrackerKeys.KEY_GAP_CLOSING_MAX_FRAME_GAP,(Integer) 1)
         settings.trackerSettings.put(TrackerKeys.KEY_ALLOW_TRACK_SPLITTING,false)
         settings.trackerSettings.put(TrackerKeys.KEY_ALLOW_TRACK_MERGING,false)
 
-        System.out.println(settings.toString())
-
-        def model = new Model()
-        model.setLogger(Logger.IJ_LOGGER)
-
         def trackmate = new TrackMate(model,settings)
-        trackmate.process()
 
-        IJ.log(String.valueOf(model.getSpots().getNSpots(true)))
+        if (!trackmate.checkInput()) {
+            IJ.log(trackmate.getErrorMessage())
+        }
 
+        if (!trackmate.process()) {
+            IJ.log(trackmate.getErrorMessage())
+        }
+
+        def cells = new ArrayList<Cell>()
+        def spots = model.getSpots()
+        spots.iterable(true).each {
+            def ovalRoi = new OvalRoi(it.getFeature(Spot.POSITION_X),it.getFeature(Spot.POSITION_Y),it.getFeature(Spot.RADIUS),it.getFeature(Spot.RADIUS))
+            def cell = new Cell(ovalRoi.getFloatPolygon().xpoints,ovalRoi.getFloatPolygon().ypoints,Roi.POLYGON)
+
+            cells.add(cell)
+
+        }
+
+        return cells;
+    }
+
+    ArrayList<Cell> detectFluorescentCells(ImagePlus ipl) {
+        // Applying basic image processing to clean up the image
+        new BackgroundSubtracter().rollingBallBackground(ipl.getProcessor(),50,false,false,false,true,true)
+        new GaussianBlur().blurGaussian(ipl.getProcessor(),2,2,0.01)
+
+        // Getting the stack intensity histogram
+        def stackStats = new StackStatistics(ipl)
+        def statLong = stackStats.histogram
+        def statInt = new int[statLong.length]
+
+        // Converting long-format image stack histogram measurements to int-format
+        for (int i=0;i<statLong.length;i++) {
+            statInt[i] = Math.toIntExact(statLong[i])
+        }
+
+        // Determining the threshold (Huang method)
+        def thresh = new AutoThresholder().getThreshold(AutoThresholder.Method.Huang,statInt)
+
+        // Running the threshold on each image in the stack
+        for (int i=0;i<ipl.getNSlices();i++) {
+            ipl.setSlice(i)
+            ipl.getProcessor().threshold(thresh)
+
+        }
+
+        // Creating a container for the cells and a RoiManager, where the results will be stored temporarily
+        def cells = new ArrayList<Cell>()
+        def rois = new RoiManager()
+        rois.setVisible(false)
+
+        // Running through each slice (frame), detecting the particles
+        for (int slice=0;slice<ipl.getNSlices();slice++) {
+            // Running analyse particles
+            def partAnal = new ParticleAnalyzer(0, Measurements.AREA | Measurements.CENTROID, null, 300, Integer.MAX_VALUE, 0, 1)
+            partAnal.setRoiManager(rois)
+            ipl.setSlice(slice)
+            partAnal.analyze(ipl)
+
+            // Adding all current rois to the cell arraylist
+            for (int i = 0; i < rois.count; i++) {
+                def cell = new Cell(rois.getRoi(i).polygon, PolygonRoi.POLYGON)
+                cell.setPosition(0,0,slice)
+                cells.add(cell)
+            }
+
+            // Removing the previously-detected ROIs from the RoiManager
+            rois.reset()
+        }
+
+        return cells
+
+    }
+}
+
+class Cell extends PolygonRoi{
+
+    Cell(int[] xPoints, int[] yPoints, int nPoints, int type) {
+        super(xPoints, yPoints, nPoints, type)
+    }
+
+    Cell(float[] xPoints, float[] yPoints, int nPoints, int type) {
+        super(xPoints, yPoints, nPoints, type)
+    }
+
+    Cell(float[] xPoints, float[] yPoints, int type) {
+        super(xPoints, yPoints, type)
+    }
+
+    Cell(Polygon p, int type) {
+        super(p, type)
+    }
+
+    Cell(FloatPolygon p, int type) {
+        super(p, type)
+    }
+
+    Cell(int sx, int sy, ImagePlus imp) {
+        super(sx, sy, imp)
     }
 }
 
